0a90d41b2612bbc63164493b8b814fc59db9a52f
Linus Basig <linus@basig.me>
2019-09-28 19:12:05 +0200

    feat: event importer

---
+++ backend/events/management/commands/import_events.py
@@ -0,0 +1,45 @@
+from django.core.management.base import BaseCommand
+from django.contrib.gis.geos import Point
+from events.models import Event, EventCategory
+import json
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        with open("data/guidle/category.json", "r") as file:
+            categories = json.load(file)
+
+        for category in categories:
+            category_id = category["category_id"]
+            category_name = category["title_de"]
+
+            EventCategory.objects.get_or_create(id=category_id, name=category_name)
+
+        with open("data/guidle/event.json", "r") as file:
+            events = json.load(file)
+
+        for event in events:
+            event_id = event["event_id"]
+            event_date = event["date"]
+            start_time = event["start_time"]
+            end_time = event["end_time"]
+            event_name = event["title_de"][0:100]
+            event_venue = event["address_venue_name"][0:100]
+            coordinates = Point(event["address_latitude"], event["address_longitude"])
+
+            event, _ = Event.objects.get_or_create(id=event_id)
+            event.event_name = event_name
+            event.venue_name = event_venue
+            event.date = event_date
+            event.start_time = start_time
+            event.end_time = end_time
+            event.coordinates=coordinates
+            event.save()
+
+        with open("data/guidle/event_category.json", "r") as file:
+            mappings = json.load(file)
+
+        for mapping in mappings:
+            event, _ = Event.objects.get_or_create(id=mapping["event_id"])
+            category, _ = EventCategory.objects.get_or_create(id=mapping["category_id"])
+
+            event.categories.add(category)
