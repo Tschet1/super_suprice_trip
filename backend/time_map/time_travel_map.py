4fcce2178a27fa159e99990606130b17f0e1b903
Linus Basig <linus@basig.me>
2019-09-28 17:16:26 +0200

    feat: add polygon query test command

--- backend/time_map/time_travel_map.py
+++ backend/time_map/time_travel_map.py
@@ -1,12 +1,9 @@
 import requests
 import json
 from datetime import datetime
-from shapely.geometry import Point
-from shapely.geometry.polygon import Polygon
 import os
-import dotenv
 from collections import namedtuple
-dotenv.load_dotenv()
+from django.contrib.gis.geos import Polygon, LinearRing
 
 Coordinates = namedtuple('Coordinates', ['long', 'lat'])
 
@@ -44,23 +41,10 @@ def get_travel_time(departure_time, max_travel_time_min, coordinates):
     response = requests.request("POST", url, data=payload, headers=headers)
 
     res = json.loads(response.text)
-
-    return [Polygon(
-        shell=[Coordinates(long=point['lng'], lat=point['lat']) for point in shape['shell']],
-        holes=[(point['lat'], point['lng']) for point in shape['holes']],
-    ) for shape in res['results'][0]['shapes']]
-
-def is_point_in_shape(shape, coordinates):
-    return shape.contains(Point(coordinates.long, coordinates.lat))
-
-if __name__ == '__main__':
-    coordinates_zug = Coordinates(lat=47.166168, long=8.515495)
-    coordinates_zurich = Coordinates(lat=47.36667, long=8.55)
-
-    time_distance = get_travel_time(datetime.now(), 90, coordinates_zug)[0]
-
-    print(is_point_in_shape(time_distance, coordinates_zug))
-    print(is_point_in_shape(time_distance, coordinates_zurich))
+    shape = res['results'][0]['shapes'][0]
+    ext_coords = LinearRing([(point['lng'], point['lat']) for point in shape['shell']])
+    int_coords = LinearRing([(point['lng'], point['lat']) for point in shape['holes']])
+    return Polygon(ext_coords) 
 
 
 
